58317d0c2d88ac1572ec96d5b49b1f6759114e6e
Blue000Black <129663844+Blue000Black@users.noreply.github.com>
2025-05-12 21:35:46 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import *
 import os
 from random import randint
 import socket
@@ -6,26 +6,53 @@ import socket
 
 if __name__ == '__main__':
     ip = socket.gethostbyname(socket.gethostname())
-    port = 5000
+    port = 8080
     app = Flask(__name__)
     memes = list(name for name in os.listdir('static/img'))
-    meme = memes[randint(0, len(memes))]
+    meme = memes[randint(0, len(memes) - 1)]
     print(meme)
     style = open('static/css/style.css', 'r').read()
-    @app.route('/')
+    @app.route('/', methods=['POST', 'GET'])
     def index():
-        return f"""<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <title>MemeCards</title>
-                      </head>
-                      <body>
-                        <h1>MemeCards</h1>
-                        <img src="{url_for('static', filename='img/' + meme)}">
-                      </body>
-                    <style>
-                    {style}
-                    <style>
-                    </html>"""
+        global meme
+        global memes
+        if request.method == 'GET':
+            return f"""<!doctype html>
+                        <html lang="en">
+                            <head>
+                                <meta charset="utf-8">
+                                <title>MemeCards</title>
+                            </head>
+                            <body>
+                                <form method="post">
+                                <h1>MemeCards</h1>
+                                <img src="{url_for('static', filename='img/' + meme)}">
+                                <br></br>
+                                <button class="update" type="submit">Новая картинка<button>
+                                </form>
+                            </body>
+                            <style>
+                                {style}
+                            <style>
+                        </html>"""
+        if request.method == 'POST':
+            meme = memes[randint(0, len(memes) - 1)]
+            return f"""<!doctype html>
+                       <html lang="en">
+                           <head>
+                               <meta charset="utf-8">
+                               <title>MemeCards</title>
+                           </head>
+                           <body>
+                               <form method="post">
+                               <h1>MemeCards</h1>
+                               <img src="{url_for('static', filename='img/' + meme)}">
+                               <br></br>
+                               <button class="update" type="submit">Новая картинка<button>
+                               </form>
+                           </body>
+                           <style>
+                               {style}
+                           <style>
+                       </html>"""
     app.run(host=ip, port=port)
