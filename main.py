c8ee2e783d9457b63c3e39cec35dbd7c1d4d04f4
Blue000Black <129663844+Blue000Black@users.noreply.github.com>
2025-05-12 16:15:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 # pip install config
 import logging
 from telegram.ext import Application, MessageHandler, filters
+from flask import Flask
+import os
 from random import randint
+import socket
 
 
 logging.basicConfig(
@@ -11,24 +14,36 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 async def answer(update, context):
-    answers = [
-        '1',
-        '2',
-        '3',
-        '4',
-        '5',
-        '6',
-        '7'
-    ]
-    await update.message.reply_text(answers[randint(0, len(answers) - 1)])
+    global port
+    global ip
+    await update.message.reply_text(f'http://{ip}:{port}')
 
 
-def main():
+if __name__ == '__main__':
+    ip = socket.gethostbyname(socket.gethostname())
+    port = 5000
+    app = Flask(__name__)
+    memes = list(name for name in os.listdir(os.getcwd() + '/imgs'))
+    meme = memes[randint(0, len(memes))]
+    style = open('style.css', 'r').read()
+    @app.route('/')
+    def index():
+        return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>MemeCards</title>
+                      </head>
+                      <body>
+                        <h1>MemeCards</h1>
+                        <img src="imgs/{meme}">
+                      </body>
+                    <style>
+                    {style}
+                    <style>
+                    </html>"""
+    app.run(host=ip, port=port)
     application = Application.builder().token('7937526409:AAEuaGYvZ5il_vwTe4WXrfo7pAdL6XOF3XA').build()
     text_handler = MessageHandler(filters.TEXT, answer)
     application.add_handler(text_handler)
     application.run_polling()
-
-
-if __name__ == '__main__':
-    main()
