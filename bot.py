d22fa4e151677cb482c53bd0dfda882e41c53611
AleksTula <georgievskiyaleksandr@gmail.com>
2025-05-13 15:41:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,11 @@ logger = logging.getLogger(__name__)
 async def answer(update, context):
     global port
     global ip
+    global status
+    if status == 'not logined':
+        await update.message.reply_text('Введите логин и пароль в формате <логин> - <пароль>, чтобы войти, или команду /reg, чтобы зарегистрироваться')
+    elif status == 'loginned':
+        await update.message.reply_text(f'http://{ip}:{port}')
     await update.message.reply_text(f'http://{ip}:{port}')
 
 
@@ -21,4 +26,5 @@ if __name__ == '__main__':
     application = Application.builder().token('7937526409:AAEuaGYvZ5il_vwTe4WXrfo7pAdL6XOF3XA').build()
     text_handler = MessageHandler(filters.TEXT, answer)
     application.add_handler(text_handler)
+    status = 'not logined'
     application.run_polling()
